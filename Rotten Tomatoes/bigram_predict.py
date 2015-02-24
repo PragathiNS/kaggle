9ad75f8734235aabf16c9c1833ee3a9008bcb3e8
tdpetrou <petrou.theodore@gmail.com>
2015-02-23 22:29:04 -0600

    updated bigram model

--- Rotten Tomatoes/bigram_predict.py
+++ Rotten Tomatoes/bigram_predict.py
@@ -12,35 +12,60 @@ stops.remove('but')
 porter = PorterStemmer()
 
 def clean_phrase(phrase):
-	phrase = re.sub("[^a-zA-Z]",' ',  phrase.lower())
-	phrase = re.sub(" . |^. | .$",' ',  phrase.lower())
-	phrase = ' '.join([porter.stem(word) for word in phrase.split() if word not in stops])
-	return phrase
+    phrase = re.sub("[^a-zA-Z]",' ',  phrase.lower())
+    phrase = re.sub(" . |^. | .$",' ',  phrase.lower())
+    phrase = ' '.join([porter.stem(word) for word in phrase.split() if word not in stops])
+    return phrase
 
-def score_phrase(phrase, score = 0):
+def score_phrase(phrase, score = 0, total_found = 0):
+    if len(phrase) == 0:
+        return 2 if total_found ==0 else score / total_found
     if len(phrase) == 1:
-        return score + unigram_score_clean.get(phrase[0], 0)
+        temp_score = unigram_score_clean.get(phrase[0], 2)
+        if temp_score <= 1.5 or temp_score >= 2.5:
+            return (score + temp_score) / (total_found + 1)
+        else:
+            return 2 if total_found ==0 else score / total_found
     if len(phrase) == 2:
         if phrase in bigram_dict:
-            return score + bigram_dict[tuple(phrase)]
+            temp_score = bigram_dict[phrase]
+            if temp_score <= 1.5 or temp_score >= 2.5:
+                return (score + temp_score) / (total_found + 1)
+            else:
+                return 2 if total_found == 0 else score / total_found
         else:
-            return score_phrase(phrase[1:], score)
+            temp_score = unigram_score_clean.get(phrase[0], 2)
+            if temp_score <= 1.5 or temp_score >= 2.5:
+                return score_phrase(phrase[1:], score + temp_score, total_found + 1)
+            else:
+                return score_phrase(phrase[1:], score, total_found)
     else:
         if phrase[:2] in bigram_dict:
-            return score_phrase(phrase[2:], score + bigram_dict.get(tuple(phrase[:2]), 0))
+            temp_score = bigram_dict[phrase[:2]]
+            if temp_score <= 1.5 or temp_score >= 2.5:
+                return score_phrase(phrase[2:], score + temp_score, total_found + 1)
+            else:
+                return score_phrase(phrase[2:], score, total_found)
         elif phrase[0] in unigram_score_clean:
-            return score_phrase(phrase[1:], score + unigram_score_clean.get(phrase[0], 0))
+            temp_score = unigram_score_clean[phrase[0]]
+            if temp_score <= 1.5 or temp_score >= 2.5:
+                return score_phrase(phrase[1:], score + temp_score, total_found + 1)
+            else:
+                return score_phrase(phrase[1:], score, total_found)
         else:
-            return score_phrase(phrase[2:], score)
+            return score_phrase(phrase[1:], score, total_found)   
 
 train = pd.read_csv('data/train.tsv', sep='\t')
 train['clean_phrase']=train['Phrase'].apply(clean_phrase)
 train['PhraseLength'] = train['clean_phrase'].apply(lambda x: len(x.split()))
 almost_bigram_dict = train[train['PhraseLength'] == 2][['clean_phrase', 'Sentiment']]
 almost_unigram_dict = train[train['PhraseLength'] == 1][['clean_phrase', 'Sentiment']]
+unigram_score_clean = almost_unigram_dict.groupby('clean_phrase')['Sentiment'].mean().to_dict()
 bigram_dict = dict()
 for row in pd.DataFrame(almost_bigram_dict.groupby('clean_phrase')['Sentiment'].mean()).reset_index().values:
-	bigram_dict[tuple(row[0].split())] = row[1]
-unigram_score_clean = almost_unigram_dict.groupby('clean_phrase')['Sentiment'].mean().to_dict()
+    bigram_dict[tuple(row[0].split())] = row[1]
 
-print bigram_dict
+test = pd.read_csv('data/test.tsv', sep='\t')
+test['clean_phrase']=test['Phrase'].apply(clean_phrase)
+test['bigram_score'] = test['clean_phrase'].apply(lambda x: int(round(score_phrase(tuple(x.split())))))
+test[['PhraseId', 'bigram_score']].rename(columns={'bigram_score' : 'Sentiment'}).to_csv('data/bigram_predictor.csv', index=False)
